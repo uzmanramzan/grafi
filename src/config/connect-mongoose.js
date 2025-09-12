5d66ed0bf9d0865389f702f7b38a6674cd627ad9
uzmanR <uzmanramzan@gmail.com>
2025-09-12 23:44:49 +0500

    changes

--- src/config/connect-mongoose.js
+++ src/config/connect-mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 (async () => {
   try {
-    console.log("db connected====");
+    console.log("db connected===");
     mongoose.set('strictQuery', false)
     await mongoose.connect(process.env.DATABASE_URL || 'mongodb+srv://abdullah:12345@pixelcluster.zzbjvjd.mongodb.net/solace?retryWrites=true&w=majority');
     console.log("db connected");
